08a058cd2bc93c4f621346c3c3d82ad35041af53
Memka <mkasenova87@gmail.com>
2023-01-24 22:08:09 +0600

    дополнено

--- DZ #7/main.swift
+++ DZ #7/main.swift
@@ -11,20 +11,27 @@ import Foundation
 
 //Создать словарь из ключа имя пользователя и значения пароль, заполнить данными. Создать функцию авторизация, в котором 2 ридлайна для ввода информации. Сделать так чтобы при правильном вводе информации выводить "вы успешно вошли". Если имя пользователя нет в словаре, то "вы успешно зарегистрировались",  учитывайте что пароль должен быть не менее 6 символов. Если имя пользователя есть, но неправильно вводится пароль, то "неправильный пароль"
 
-var avtoPassword = ["Dima":123456, "Helena":319999, "Aleks":323569]
+var avtoPassword = ["Dima":"123456", "Helena":"319999", "Aleks":"323569"]
 
-func avtorization (){
-    print("Введите Логин:")
-    var loginid = readLine()
+func authorization(username: String, password: String) {
+    var result: String?
+    guard password.count >= 6 else {
+        print("Пароль должен быть не менее 6 символов.")
+        return
+    }
     
-    print("Введите пароль:")
-    var avtoriz = readLine()
-    loginid = avtoriz
-    
-    
-    for (key, value) in avtoPassword {
-        if key == avtoPassword () {
-            print("Вы успешно вошли!")
+    for users in avtoPassword {
+        if username == users.key && password == users.value {
+            result = "Вы успешно вошли"
+            break
+        } else if username == users.key {
+            result = "Неправильный пароль"
+            break
+        } else {
+            result = "Вы успешно зарегистрировались"
         }
     }
+    print(result!)
 }
+
+authorization(username: readLine()!, password: readLine()!)
